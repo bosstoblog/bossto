726bb848b8d2ed563b020fb9d9f6a7dbd0e06883
Dima <dima@aetlas.pro>
2026-03-15 20:15:50 +0300

    feat: i18n, GitHub Actions deploy, minimal landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://bossto.blog',
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru', 'en'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+});
